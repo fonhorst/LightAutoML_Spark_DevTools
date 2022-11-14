99899459e99ec4e4483453c8d7b230340de18045
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-11-14 20:45:21 +0300

    added logging to mlflow

--- scenarios/examples-spark/spark-ml-pipe-lgb-light.py
+++ scenarios/examples-spark/spark-ml-pipe-lgb-light.py
@@ -1,6 +1,7 @@
 import logging.config
 import logging.config
 import os
+import uuid
 
 import mlflow
 from pyspark.ml import PipelineModel
@@ -17,7 +18,9 @@ from sparklightautoml.tasks.base import SparkTask as SparkTask
 from sparklightautoml.utils import logging_config, VERBOSE_LOGGING_FORMAT, log_exec_time, log_exec_timer
 from sparklightautoml.validation.iterators import SparkFoldsIterator
 
-logging.config.dictConfig(logging_config(level=logging.INFO, log_filename='/tmp/slama.log'))
+uid = uuid.uuid4()
+log_filename = f'/tmp/slama-{uid}.log'
+logging.config.dictConfig(logging_config(level=logging.DEBUG, log_filename=log_filename))
 logging.basicConfig(level=logging.DEBUG, format=VERBOSE_LOGGING_FORMAT)
 logger = logging.getLogger(__name__)
 
@@ -25,95 +28,104 @@ logger = logging.getLogger(__name__)
 # Run ./bin/download-datasets.sh to get required datasets into the folder.
 
 
-def main():
-    with open("/tmp/slama.log") as f:
-        log_content = f.read()
-    mlflow.log_text(log_content, "slama.log")
+def main(cv: int, seed: int, dataset_name: str = "lama_test_dataset"):
+    spark = get_spark_session()
 
-# def main():
-#     spark = get_spark_session()
-#
-#     seed = 42
-#     cv = 5
-#     dataset_name = "lama_test_dataset"
-#     path, task_type, roles, dtype = get_dataset_attrs(dataset_name)
-#
-#     persistence_manager = get_persistence_manager()
-#
-#     with log_exec_time():
-#         train_df, test_df = prepare_test_and_train(spark, path, seed)
-#
-#         task = SparkTask(task_type)
-#         score = task.get_dataset_metric()
-#
-#         sreader = SparkToSparkReader(task=task, cv=cv, advanced_roles=False)
-#         sdataset = sreader.fit_read(train_df, roles=roles, persistence_manager=persistence_manager)
-#
-#         iterator = SparkFoldsIterator(sdataset).convert_to_holdout_iterator()
-#
-#         spark_ml_algo = SparkBoostLGBM(freeze_defaults=False, use_single_dataset_mode=True)
-#         spark_features_pipeline = SparkLGBSimpleFeatures()
-#
-#         ml_pipe = SparkMLPipeline(
-#             ml_algos=[spark_ml_algo],
-#             pre_selection=None,
-#             features_pipeline=spark_features_pipeline,
-#             post_selection=None
-#         )
-#
-#         with log_exec_timer("fit_time") as fit_time:
-#             oof_preds_ds = ml_pipe.fit_predict(iterator)
-#             oof_score = score(oof_preds_ds[:, spark_ml_algo.prediction_feature])
-#
-#             logger.info(f"OOF score: {oof_score}")
-#
-#         mlflow.log_param(fit_time.name, fit_time.duration)
-#         mlflow.log_param("oof_score", oof_score)
-#
-#         # 1. first way (LAMA API)
-#         with log_exec_timer("predict") as predict_time:
-#             test_sds = sreader.read(test_df, add_array_attrs=True)
-#             test_preds_ds = ml_pipe.predict(test_sds)
-#             test_score = score(test_preds_ds[:, spark_ml_algo.prediction_feature])
-#
-#             logger.info(f"Test score (#1 way): {test_score}")
-#
-#         mlflow.log_param(predict_time.name, predict_time.duration)
-#         mlflow.log_param("test_score", test_score)
-#
-#         # 2. second way (Spark ML API, save-load-predict)
-#         with log_exec_timer("model_saving") as save_time:
-#             transformer = PipelineModel(stages=[sreader.transformer(add_array_attrs=True), ml_pipe.transformer()])
-#             transformer.write().overwrite().save("/tmp/reader_and_spark_ml_pipe_lgb")
-#
-#         mlflow.log_param(save_time.name, save_time.duration)
-#
-#         with log_exec_timer("model_loading") as load_time:
-#             pipeline_model = PipelineModel.load("/tmp/reader_and_spark_ml_pipe_lgb")
-#
-#         mlflow.log_param(load_time.name, load_time.duration)
-#
-#         test_pred_df = pipeline_model.transform(test_df)
-#         test_pred_df = test_pred_df.select(
-#             SparkDataset.ID_COLUMN,
-#             F.col(roles['target']).alias('target'),
-#             F.col(spark_ml_algo.prediction_feature).alias('prediction')
-#         )
-#         test_score = score(test_pred_df)
-#         logger.info(f"Test score (#3 way): {test_score}")
-#
-#     logger.info("Finished")
-#
-#     spark.stop()
+    mlflow.log_param("application_id", spark.sparkContext.applicationId)
+    mlflow.log_param("executors", spark.conf.get("spark.executor.instances", None))
+    mlflow.log_param("executor_cores", spark.conf.get("spark.executor.cores", None))
+    mlflow.log_param("executor_memory", spark.conf.get("spark.executor.memory", None))
+    mlflow.log_param("partitions_nums", spark.conf.get("spark.default.parallelism", None))
+    mlflow.log_param("bucket_nums", os.environ.get("BUCKET_NUMS", None))
+    mlflow.log_dict(spark.conf, "spark_conf.json")
+
+    path, task_type, roles, dtype = get_dataset_attrs(dataset_name)
+
+    persistence_manager = get_persistence_manager()
+
+    with log_exec_time():
+        train_df, test_df = prepare_test_and_train(spark, path, seed)
+
+        task = SparkTask(task_type)
+        score = task.get_dataset_metric()
+
+        sreader = SparkToSparkReader(task=task, cv=cv, advanced_roles=False)
+        sdataset = sreader.fit_read(train_df, roles=roles, persistence_manager=persistence_manager)
+
+        iterator = SparkFoldsIterator(sdataset).convert_to_holdout_iterator()
+
+        spark_ml_algo = SparkBoostLGBM(freeze_defaults=False, use_single_dataset_mode=True)
+        spark_features_pipeline = SparkLGBSimpleFeatures()
+
+        ml_pipe = SparkMLPipeline(
+            ml_algos=[spark_ml_algo],
+            pre_selection=None,
+            features_pipeline=spark_features_pipeline,
+            post_selection=None
+        )
+
+        with log_exec_timer("fit_time") as fit_time:
+            oof_preds_ds = ml_pipe.fit_predict(iterator)
+            oof_score = score(oof_preds_ds[:, spark_ml_algo.prediction_feature])
+
+            logger.info(f"OOF score: {oof_score}")
+
+        mlflow.log_metric(fit_time.name, fit_time.duration)
+        mlflow.log_metric("oof_score", oof_score)
+
+        # 1. first way (LAMA API)
+        with log_exec_timer("predict") as predict_time:
+            test_sds = sreader.read(test_df, add_array_attrs=True)
+            test_preds_ds = ml_pipe.predict(test_sds)
+            test_score = score(test_preds_ds[:, spark_ml_algo.prediction_feature])
+
+            logger.info(f"Test score (#1 way): {test_score}")
+
+        mlflow.log_metric(predict_time.name, predict_time.duration)
+        mlflow.log_metric("test_score", test_score)
+
+        model_path = f"/tmp/models/spark-ml-pipe-lgb-light-{uid}"
+        # 2. second way (Spark ML API, save-load-predict)
+        with log_exec_timer("model_saving") as save_time:
+            transformer = PipelineModel(stages=[sreader.transformer(add_array_attrs=True), ml_pipe.transformer()])
+            transformer.write().overwrite().save(model_path)
+
+        mlflow.log_metric(save_time.name, save_time.duration)
+        mlflow.log_param("model_path", model_path)
+
+        with log_exec_timer("model_loading") as load_time:
+            pipeline_model = PipelineModel.load(model_path)
+
+        mlflow.log_metric(load_time.name, load_time.duration)
+
+        test_pred_df = pipeline_model.transform(test_df)
+        test_pred_df = test_pred_df.select(
+            SparkDataset.ID_COLUMN,
+            F.col(roles['target']).alias('target'),
+            F.col(spark_ml_algo.prediction_feature).alias('prediction')
+        )
+        test_score = score(test_pred_df)
+        logger.info(f"Test score (#3 way): {test_score}")
+
+    logger.info("Finished")
+
+    spark.stop()
+
+    log_files = bool(int(os.environ.get("LOG_FILES_TO_MLFLOW", "0")))
+    if log_files:
+        mlflow.log_artifact(log_filename, "run.log")
 
 
 if __name__ == "__main__":
     log_to_mlflow = bool(int(os.environ.get("LOG_TO_MLFLOW", "0")))
+    dataset_name = os.environ.get("DATASET", "lama_test_dataset")
+    seed = int(os.environ.get("SEED", "42"))
+    cv = int(os.environ.get("CV", "5"))
 
     if log_to_mlflow:
         exp_id = os.environ.get("EXPERIMENT", None)
         assert exp_id, "EXPERIMENT should be set if LOG_TO_MLFLOW is true"
         with mlflow.start_run(experiment_id=exp_id) as run:
-            main()
+            main(cv, seed, dataset_name)
     else:
-        main()
+        main(cv, seed, dataset_name)
