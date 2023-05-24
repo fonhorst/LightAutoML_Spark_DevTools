dc64fe5af8d7c33ffcb8268c72fa3a317774ee11
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-05-24 15:51:59 +0300

    successfult test for simple agg for saving preffered locations for scheduling

--- scenarios/examples-spark/coalescing-map-reduce.py
+++ scenarios/examples-spark/coalescing-map-reduce.py
@@ -1,12 +1,13 @@
 import logging.config
+import logging.config
 import math
 import os
-import time
 
+from pyspark.sql import functions as sf
 from sparklightautoml.computations.manager import get_executors, PrefferedLocsPartitionCoalescerTransformer
 from sparklightautoml.dataset.base import SparkDataset
 from sparklightautoml.dataset.persistence import PlainCachePersistenceManager
-from sparklightautoml.utils import logging_config, VERBOSE_LOGGING_FORMAT
+from sparklightautoml.utils import logging_config, VERBOSE_LOGGING_FORMAT, JobGroup
 
 from examples_utils import get_spark_session
 
@@ -33,12 +34,25 @@ def main():
     execs_per_slot = max(1, math.floor(len(execs) / parallelism))
     slots_num = int(len(execs) / execs_per_slot)
 
+    coalesce_dfs = []
+    pref_locs_for_dfs = []
     for i in range(slots_num):
-        logger.info(f"Slot num #{i}")
         pref_locs = execs[i * execs_per_slot: (i + 1) * execs_per_slot]
+        logger.info(f"Slot num #{i}, pref locs: {pref_locs}")
         coalesced_data = PrefferedLocsPartitionCoalescerTransformer(pref_locs=pref_locs) \
             .transform(ds.data).cache()
-        coalesced_data.write.mode('overwrite').format('noop').save()
+        coalesced_data.write.format("noop").mode("overwrite").save()
+        coalesce_dfs.append(coalesced_data)
+        pref_locs_for_dfs.append(pref_locs)
+
+    for i, df in enumerate(coalesce_dfs):
+        logger.info(f"Calculating for slot #{i}")
+        with JobGroup(f"Agg slot #{i}", f"Should be executed on {pref_locs_for_dfs[i]}", spark):
+            df.agg(sf.count('*').alias('count')).write.format('noop').mode('overwrite').save()
+
+    # a trick for execution optimization
+    # all_df = functools.reduce(lambda acc, df: acc.unionByName(df), coalesce_dfs)
+    # all_df.count()
 
     # sleep_time_secs = 600
     # logger.info(f"Sleeping for the timeout {sleep_time_secs} until spark stopping")
