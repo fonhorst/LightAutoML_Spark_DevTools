161c1aa87bcb2bd8c5d54f550387e7f7bfaf92bb
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-05-25 19:30:21 +0300

    successful example of running with multiple resource profiles from several threads submitting jobs in parallel (application_1685028740051_0009)

--- scenarios/examples-spark/stage-level-scheduling.py
+++ scenarios/examples-spark/stage-level-scheduling.py
@@ -1,6 +1,8 @@
 import os
 import time
+from multiprocessing.pool import ThreadPool
 
+from pyspark import inheritable_thread_target
 from pyspark.resource import ResourceProfileBuilder, ExecutorResourceRequests, TaskResourceRequests
 from pyspark.sql import SparkSession
 from sparklightautoml.dataset.base import SparkDataset
@@ -11,6 +13,24 @@ from sparklightautoml.utils import JobGroup
 def map_partitions(x):
     time.sleep(60)
     return [sum(1 for _ in x)]
+
+
+def build_task(spark: SparkSession, i: int, ds: SparkDataset):
+    def func():
+        rpb = ResourceProfileBuilder()
+        ereq = ExecutorResourceRequests().cores(4).memory("6g").memoryOverhead("2g")
+        treq = TaskResourceRequests().cpus(4)
+        rpb.require(ereq)
+        rpb.require(treq)
+        rp = rpb.build
+        # Alternative 1
+        with JobGroup(f"Run #{i}", f"Calculating with Resource Profile #{rp.id}", spark):
+            # barrier mode is not supported with dynamic allocation
+            mapped_data_rdd = ds.data.rdd.withResources(profile=rp).mapPartitions(map_partitions)
+            result = mapped_data_rdd.collect()
+            print(f"Result: {result}")
+        return f"Ready #{i}"
+    return func
 
 
 def main():
@@ -35,19 +55,13 @@ def main():
         persistence_manager=PlainCachePersistenceManager()
     )
 
-    for i in range(2):
-        rpb = ResourceProfileBuilder()
-        ereq = ExecutorResourceRequests().cores(4).memory("6g").memoryOverhead("2g")
-        treq = TaskResourceRequests().cpus(4)
-        rpb.require(ereq)
-        rpb.require(treq)
-        rp = rpb.build
-        # Alternative 1
-        with JobGroup(f"Run #{i}", f"Calculating with Resource Profile #{rp.id}", spark):
-            # barrier mode is not supported with dynamic allocation
-            mapped_data_rdd = ds.data.rdd.withResources(profile=rp).mapPartitions(map_partitions)
-            result = mapped_data_rdd.collect()
-            print(f"Result: {result}")
+    tasks = [build_task(spark, i, ds) for i in range(2)]
+
+    pool = ThreadPool(processes=2)
+    tasks = map(inheritable_thread_target, tasks)
+    results = [result for result in pool.imap_unordered(lambda f: f(), tasks)]
+
+    print(results)
 
     # Alternative 2
     # result = spark.createDataFrame(mapped_data_rdd.map(lambda x: (x,)))
