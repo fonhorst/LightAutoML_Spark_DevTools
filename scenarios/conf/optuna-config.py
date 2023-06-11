a1d3bed827d22829ee9279d9c67f8753f5fc213b
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-06-11 23:36:29 +0300

    add commands to scenarioctl and fixing naming in slama-repeater-job

--- scenarios/conf/optuna-config.py
+++ scenarios/conf/optuna-config.py
@@ -1,10 +1,11 @@
 import itertools
 
-parallelism_degrees = ["1", "2", "4", "8", "16"]
+parallelism_degrees = ["1", "2", "4"]#, "8", "16"]
 
 datasets = [
-    "synth_10kk_100",
-    "synth_5kk_100"
+    # "synth_10kk_100",
+    # "synth_5kk_100"
+    "lama_test_dataset"
 ]
 
 configurations = {
@@ -23,7 +24,7 @@ configurations = {
                 "DRIVER_CORES": "6",
                 "DRIVER_MEMORY": "16g",
                 "DRIVER_MAX_RESULT_SIZE": "5g",
-                "EXECUTOR_INSTANCES": "16",
+                "EXECUTOR_INSTANCES": "4",
                 "EXECUTOR_CORES": "4",
                 "EXECUTOR_MEMORY": "40g",
                 "PYSPARK_PYTHON_PATH": "/python_envs/.replay_venv/bin/python3.9",
